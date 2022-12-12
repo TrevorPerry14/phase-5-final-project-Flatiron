be8bbc72b2ba969a808a63fe0c1225c4f2206ad3
Trevor <trevorperry143@gmail.com>
2022-12-12 17:50:17 -0600

    small changes for render

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ module BackendProject
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.use ActionDispatchP::Cookies
+    config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore 
 
     config.action_dispatch.cookies_same_site_protection = :strict
